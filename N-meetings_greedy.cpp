a91ee0170259fd249edad7c4c8a3b2030f21f287
kukkadapudheeraj <kukkadapudheeraj@gmail.com>
2023-04-07 10:21:42 -0400

    Create N-meetings_greedy.cpp

---
+++ N-meetings_greedy.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    int maxMeetings(int start[], int end[], int n)
+    {
+        // Your code here
+        pair<int,int> pr[n];
+        for(int i=0;i<n;i++)
+        {
+            pr[i].first=end[i];
+            pr[i].second=start[i];
+        }
+        sort(pr,pr+n);
+        int c=1;
+        int en=pr[0].first;
+        for(int i=1;i<n;i++)
+        {
+            if(pr[i].second>en)
+            {
+                c++;
+                en=pr[i].first;
+            }
+        }
+        return c;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int start[n], end[n];
+        for (int i = 0; i < n; i++) cin >> start[i];
+
+        for (int i = 0; i < n; i++) cin >> end[i];
+
+        Solution ob;
+        int ans = ob.maxMeetings(start, end, n);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+
+
+
+/*
+Input:
+N = 6
+start[] = {1,3,0,5,8,5}
+end[] =  {2,4,6,7,9,9}
+Output: 
+4
+Explanation:
+Maximum four meetings can be held with
+given start and end timings.
+The meetings are - (1, 2),(3, 4), (5,7) and (8,9)
+*/
